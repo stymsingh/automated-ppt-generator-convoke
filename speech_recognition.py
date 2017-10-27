d033449398b2e7d12d4e533c2f287fb20c952a9f
Satyam Singh <anonymoussatyam1997@hotmail.com>
2017-10-28 03:35:21 +0530

    added IBM WATSON text to speech files codes

---
+++ speech_recognition.py
@@ -0,0 +1,15 @@
+import pyaudio
+import speech_recognition as sr
+
+index = pyaudio.PyAudio().get_device_count() - 1
+print( index)
+
+r = sr.Recognizer()
+for i in range(5):
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+
+try:
+    print("You said " + r.recognize(audio))
+except LookupError:
+    print("Could not understand audio")
